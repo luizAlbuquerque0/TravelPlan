b070097662462b345ddc3dd81256c9f9ecec65ce
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2022-11-11 11:28:01 -0300

    Migrations Settings

--- TravelPlan.API/Program.cs
+++ TravelPlan.API/Program.cs
@@ -1,6 +1,20 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TravelPlan.Application.Commands.CreateViagem;
+using TravelPlan.Core.Repositories;
+using TravelPlan.Infrastructure.Persistence;
+using TravelPlan.Infrastructure.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("TravelPlanCs");
+builder.Services.AddDbContext<TravelPlanDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddMediatR(typeof(CreateViagemCommand));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IViagemRepository, ViagemRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
