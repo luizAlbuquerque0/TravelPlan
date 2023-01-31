0ef5cb62f5e86e898ad8d482fc636cf9c970a2de
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2023-01-31 15:35:48 -0300

    feature/implement-activitiesController

---
+++ TravelPlan.API/Controllers/ActivitiesControllers.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TravelPlan.Application.Queries.GetAtividadeByDate;
+
+namespace TravelPlan.API.Controllers
+{
+    [Route("api/activities")]
+    public class ActivitiesControllers : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ActivitiesControllers(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetActivitiesByDate([FromBody] GetAtividadeByDateQuery query)
+        {
+            var activities = await _mediator.Send(query);
+            if (activities == null) return NotFound();
+
+            return Ok(activities);
+        }
+    }
+}
